8fdd4de70e6d8af8db6d4e90015a06494cea95a7
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-04-16 10:44:32 +0100

    feat:home page iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import RandomBeersPage from "./pages/RandomBeerPage";
 import AddBeerPage from "./pages/AddBeerPage";
 import BeerDetailsPage from "./pages/BeerDetailsPage";
 import "./App.css";
+import Navbar from "./components/Navbar"
 
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>LAB | React IronBeers</h1>
 
+      
+
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
+        <Route path="*" element={<HomePage/>}/>
         <Route path="/beers" element={<AllBeersPage/>}/>
         <Route path="/random-beer" element={<RandomBeersPage/>}/>
         <Route path="/new-beer" element={<AddBeerPage/>}/>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,43 @@
-function HomePage() {}
+import imgAllBeers from '../assets/beers.png';
+import imgRandomBeer from '../assets/random-beer.png';
+import imgNewBeer from '../assets/new-beer.png';
+import { Link } from 'react-router-dom';
+
+
+
+
+function HomePage() {
+    return (
+        <div className='container d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
+            <div className='row'>
+                <div className='col-md-4 text-center'>
+                    <img src={imgAllBeers} alt="" className="img-fluid mx-auto mb-3" />
+                    <Link to={'/beers'} className='nav-link'>
+                        <h5 className='text-decoration-none '>All Beers</h5>
+                    </Link>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda voluptates incidunt dignissimos itaque quis quibusdam optio odit repellendus. Eligendi laborum velit corporis asperiores error sint facere eius fugit maxime! Quo? Modi, vel sapiente maxime fuga cum nesciunt voluptatem veniam doloremque minus laboriosam repellendus? Ipsam accusamus quaerat nisi, aliquam nam voluptatum dignissimos! Veniam, officia consequuntur. Impedit ab aut minima maxime a!</p>
+                </div>
+                <div className='col-md-4 text-center'>
+                    <img src={imgRandomBeer} alt="" className="img-fluid mx-auto mb-3" />
+                    <Link to={'/random-beer'} className='nav-link'>
+                        <h5 className='text-decoration-none '>Random beer</h5>
+                    </Link>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda voluptates incidunt dignissimos itaque quis quibusdam optio odit repellendus. Eligendi laborum velit corporis asperiores error sint facere eius fugit maxime! Quo? Modi, vel sapiente maxime fuga cum nesciunt voluptatem veniam doloremque minus laboriosam repellendus? Ipsam accusamus quaerat nisi, aliquam nam voluptatum dignissimos! Veniam, officia consequuntur. Impedit ab aut minima maxime a!</p>
+                </div>
+                <div className='col-md-4 text-center'>
+                    <img src={imgNewBeer} alt="" className="img-fluid mx-auto mb-3" />
+                    <Link to={'/new-beer'} className='nav-link'>
+                        <h5 className='text-decoration-none '>New beer</h5>
+                    </Link>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda voluptates incidunt dignissimos itaque quis quibusdam optio odit repellendus. Eligendi laborum velit corporis asperiores error sint facere eius fugit maxime! Quo? Modi, vel sapiente maxime fuga cum nesciunt voluptatem veniam doloremque minus laboriosam repellendus? Ipsam accusamus quaerat nisi, aliquam nam voluptatum dignissimos! Veniam, officia consequuntur. Impedit ab aut minima maxime a!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default HomePage;
+
+
+
+
